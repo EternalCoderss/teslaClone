f87cff84d84c76e1ccf15158c2668955364daf76
eternalcoderss <shivamchoudhary602@Dmail.com>
2022-08-07 15:04:14 +0530

    Car item Component

--- App.js
+++ App.js
@@ -8,7 +8,11 @@ export default function App() {
   return (
     <View style={styles.container}>
 
-      <CarItem/>
+      <CarItem name={'Model S'}
+      tagline={'Order Online for'}
+      taglineCTA={'Touchless Delivery'}
+      image={require('./assets/images/ModelS.jpeg')}
+      />
       <StatusBar style="auto" />
     </View>
   );
--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -3,16 +3,24 @@ import {View, Text, ImageBackground} from 'react-native';
 import StyledButton from '../StyledButton';
 import styles from './style';
 
-function CarItem() {
+function CarItem(props) {
+const {name, tagline, taglineCTA,image}= props;
+
   return (
     <View style={styles.carContainer}>
-    <ImageBackground source={ require('../../assets/images/ModelS.jpeg')}
+    <ImageBackground source={image}
     style={styles.image}
     />
     <View style={styles.titles}>
-      <Text style={styles.title}>Model S</Text>
-      <Text style={styles.subtitle}>Starting at $69,420</Text>
+      <Text style={styles.title}>{name}</Text>
+      <Text style={styles.subtitle}>
+        {tagline} 
+        {' '}
+        <Text style={styles.subtitleCTA}>{taglineCTA}</Text>
+      </Text>
     </View> 
+
+    <View style = {styles.buttonContainer}>
 
     <StyledButton type = "primary" 
     content = {"Custom Order"} 
@@ -24,6 +32,8 @@ function CarItem() {
     onPress ={() =>{
       console.warn("Existing Inventory was pressed");
     }} />
+
+    </View>
   </View>
 
   );
